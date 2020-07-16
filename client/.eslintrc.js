3feb5c92d3f47e4cbcdb162958d0b1d81116e285
redfox80 <joafor2@gmail.com>
2020-07-16 12:11:39 +0200

    Second commit

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -12,6 +12,22 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+	'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "indent": [
+      "error",
+      "tab"
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
   }
 }
